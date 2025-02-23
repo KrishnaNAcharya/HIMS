fcc7801c490f06d9d3f6f5d747ae4a0922d2b8ef
KrishnaNAcharya <knacharyakavoor@gmail.com>
2025-02-23 20:56:43 +0530

    render

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const port = 5000;
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://hims-f.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
